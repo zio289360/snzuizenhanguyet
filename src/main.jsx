1afa18f1de38683743d024ddd0400d5890d73a14
zio289360 <anguyen2893@gmail.com>
2026-02-24 22:49:59 +0700

    Implement root element fallback in main.jsx
    Add fallback for root element creation in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const rootEl = document.getElementById('root') || (() => {
+  const div = document.createElement('div')
+  div.id = 'root'
+  document.body.appendChild(div)
+  return div
+})()
+
+ReactDOM.createRoot(rootEl).render(<App />)
